8a32fe9882d310e1146576ee4cb876b05c627693
mrsujith <mrsujith@gmail.com>
2016-09-26 20:07:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/just focus.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'just focus.jpg'));
+app.get('/ui/justfocus.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'justfocus.png'));
 });
 
 
