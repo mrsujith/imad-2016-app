f3cec192163b5b16197c04367d1f9e68299ef6e1
mrsujith <mrsujith@gmail.com>
2016-09-26 20:03:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,13 +13,11 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
 app.get('/ui/just focus.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'just focus.jpg'));
 });
+
+
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
