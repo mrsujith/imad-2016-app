5ca1aac395b08ae4e5831ac5a2206949f9274828
mrsujith <mrsujith@gmail.com>
2016-09-26 20:12:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,11 +13,11 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/just focus.jpg', function (req, res) {
+app.get('/ui/justfocus.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'justfocus.png'));
 });
 
-app.get('/ui/madi.jpg', function (req, res) {
+app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
