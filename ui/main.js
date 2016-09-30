ec1c707a6bcf770d4bbbd8e96c6b6f88983e8c74
mrsujith <mrsujith@gmail.com>
2016-09-30 20:34:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,11 +3,11 @@ var img = document.getElementById ('madi');
 var pos = 0;
 
 var moveright = function () {
-     pos = pos + 100 ;
+     pos = pos + 5 ;
      img.style.marginLeft = pos + 'px' ;
 }
 img.onclick = function () {
    
     
-    var interval = setInterval (moveright,10) ;
+    var interval = setInterval (moveright,100) ;
 };
