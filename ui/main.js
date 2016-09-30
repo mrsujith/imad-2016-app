4ffbe8a4cbb44b7d170dec65cf383066825bd58f
mrsujith <mrsujith@gmail.com>
2016-09-30 20:19:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 var img = document.getElementById ('madi');
 var pos = 0;
 img.onclick = function () {
-    pos = pos + 10 ;
+    pos = pos + 25 ;
     {
     img.style.marginLeft = pos + 'px' ;
     }
