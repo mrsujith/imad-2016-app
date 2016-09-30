75adb07091e990d6918049f79e3641f13427ad76
mrsujith <mrsujith@gmail.com>
2016-09-30 20:33:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,13 @@
 console.log('Loaded!');
 var img = document.getElementById ('madi');
 var pos = 0;
+
+var moveright = function () {
+     pos = pos + 100 ;
+     img.style.marginLeft = pos + 'px' ;
+}
 img.onclick = function () {
-    pos = pos + 100 ;
-    {
-    img.style.marginLeft = pos + 'px' ;
-    }
+   
+    
+    var interval = setInterval (moveright,10) ;
 };
