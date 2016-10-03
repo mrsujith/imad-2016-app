b86e48f065952c507bc252c420cdc10185c8d13b
mrsujith <mrsujith@gmail.com>
2016-10-03 19:00:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-window.aone = 0 ;
 
 app.get('/article-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
