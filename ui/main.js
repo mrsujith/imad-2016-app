602cc5114a4531ba55d080fdd103af693a4d3d4f
mrsujith <mrsujith@gmail.com>
2016-10-08 12:17:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,13 +5,9 @@ var counter = 0 ;
 button.onclick = function () {
    
          counter = counter + 1 ;
-         
          var span = document.getElementById('count1') ;
-         
          span.innerHTML = counter.toString();
-         
-    };
-
+     };
 
 
  var img = document.getElementById ('madi');
@@ -19,19 +15,15 @@ var pos = 2;
 var x = 2 ;
 var moveright = function () {
     if ( pos < 400 && -400 < pos ) {
-       
-          pos = pos + x ;
+         pos = pos + x ;
      img.style.marginLeft = pos + 'px' ;
     }
-    else {
-        x = - x ;
+    else {    x = - x ;
         pos = pos+ x ;
     }
-    
 };
 
 img.onclick = function () {
-   
         var interval = setInterval (moveright,20) ;
 }; 
 
