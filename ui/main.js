2197c83103bd807cb47611ae9982427de0230fcd
mrsujith <mrsujith@gmail.com>
2016-11-04 18:59:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,6 +19,35 @@ var button = document.getElementById('counter');
      response.send(null);
                                 };
                                 
+          var comment123 = [] ;
+          var comment = document.getElementById('comment');
+          
+             comment.onclick = function () {
+      
+       var response = new XMLHttpRequest();
+   
+    response.onreadystatechange = function () {
+       if (response.readyState === XMLHttpRequest.DONE) {
+           if (response.status === 200) {
+              var comment1 = response.responseText;
+              comment123.push(comment1);
+                 var list = '' ;
+           for (i=0;i < comment123.length ; i++) {
+                  list += "<li>" +  " Comment " + (i+1) + "  . " + comment123[i] + "..." ;
+           }
+          var ul = document.getElementById('orderlist');
+          ul.innerHTML = list ;                         }
+                                                         }
+                                                };  
+     var submit = document.getElementById('commentinput');
+     var submit1 = submit.value; 
+   
+   response.open('GET','http://mrsujith.imad.hasura-app.io/submit-name/'+ submit1,true);
+     response.send(null);
+     
+         };
+                                      
+                                
  var img = document.getElementById ('madi');
 var pos = 2;
 var x = 2 ;
