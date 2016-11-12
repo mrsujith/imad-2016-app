8f0f822c868fbeb06dfc661e1d8be912d21edc61
mrsujith <mrsujith@gmail.com>
2016-11-12 18:47:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,21 @@
+      var img = document.getElementById ('madi');
+var pos = 2;
+var x = 2 ;
+var moveright = function () {
+    if ( pos < 400 && -400 < pos ) {
+         pos = pos + x ;
+     img.style.marginLeft = pos + 'px' ;
+    }
+    else {  x = - x ;
+        pos = pos+ x ;
+    }
+};
+
+img.onclick = function () {
+        var interval = setInterval (moveright,20) ;
+}; 
+     
+     
      function todoList() {
             var item = document.getElementById("todoInput").value ;
             var text = document.createTextNode(item);
@@ -37,20 +55,5 @@ var comment123 = [] ;
         
         
                                 
- var img = document.getElementById ('madi');
-var pos = 2;
-var x = 2 ;
-var moveright = function () {
-    if ( pos < 400 && -400 < pos ) {
-         pos = pos + x ;
-     img.style.marginLeft = pos + 'px' ;
-    }
-    else {  x = - x ;
-        pos = pos+ x ;
-    }
-};
 
-img.onclick = function () {
-        var interval = setInterval (moveright,20) ;
-}; 
 
