2d6295eeb3b29d13f83d081e62a68b1c7b745a54
mrsujith <mrsujith@gmail.com>
2016-11-12 13:33:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,22 +9,16 @@ var comment123 = [] ;
        if (response.readyState === XMLHttpRequest.DONE) {
            if (response.status === 200) {
               var comment1 = response.responseText;
-                var commenting = comment1.value ;
-          var commenttext = document.createTextNode(commenting);
-              
-              comment123.push(comment1);
-                 /* var list = '' ; */
+                 comment123.push(comment1);
+                 
+                 var list = '' ; 
            for (i=0;i < comment123.length ; i++) {
-        /* list += "<li>" +  " Comment " + (i+1) + "  . " + comment123[i] + "..." ;*/
-        var newItem = document.createElement("li");
-        newItem.appendChild(comment123[i]) ;
+       list += "<li>" +  " Comment " + (i+1) + "  . " + comment123[i] + "..." ;
            }
            
-          /* var ul = document.getElementById('orderlist');
-          ul.innerHTML = list ;    */    
-          document.getElementById("orderlist").appendChild(newItem); }
-                                                         }
-                                                };  
+         var ul = document.getElementById('orderlist');
+          ul.innerHTML = list ;         }
+                                                } 
      var submit = document.getElementById('commentinput');
      var submit1 = submit.value; 
    
