419d90ae2546b92af4e2bfe06a5d7e8117c620b6
mrsujith <mrsujith@gmail.com>
2016-11-12 18:54:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,35 +24,7 @@ img.onclick = function () {
             document.getElementById("todoList").appendChild(newItem);
         }
 
-var comment123 = [] ;
-       
-       function comment() {
-      
-          var response = new XMLHttpRequest();
-   
-          response.onreadystatechange = function () {
-             if (response.readyState === XMLHttpRequest.DONE) {
-                  if (response.status === 200) {
-                       var comment1 = response.responseText;
-                       comment123.push(comment1);
-                 
-                 var list = '' ; 
-           for (i=0;i < comment123.length ; i++) {
-       list += "<li>" +  "comment " + "  . " + comment123[i] + "..." ;
-                                                 }
-           
-       var ul = document.getElementById('orderlist');
-          ul.innerHTML = list    }
-                                                }        }; 
-     var submit = document.getElementById('commentinput').value;
-     
-   response.open('GET','http://mrsujith.imad.hasura-app.io/submit-name/'+ submit,true);
-     response.send(null);
-     
-         }
-                           
-   
-        
+
         
                                 
 
