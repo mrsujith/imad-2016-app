cc217cee1097f224f069f11a1a82efc7fa98bf89
mrsujith <mrsujith@gmail.com>
2016-11-12 18:57:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -48,7 +48,12 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+
 app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
+app.get('/ui/main1.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
