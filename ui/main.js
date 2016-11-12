dce7346d9dba37a45ac04246003e7552d4175d82
mrsujith <mrsujith@gmail.com>
2016-11-12 18:36:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,6 +25,14 @@ var comment123 = [] ;
      
          }
                            
+        function todoList() {
+            var item = document.getElementById("todoInput").value ;
+            var text = document.createTextNode(item);
+            var newItem = document.createElement("li");
+            newItem.appendChild(text);
+            document.getElementById("todoList").appendChild(newItem);
+        }
+        
         
                                 
  var img = document.getElementById ('madi');
