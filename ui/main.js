6eb832c63dfd5791494a4b17fa5f75823b34855e
mrsujith <mrsujith@gmail.com>
2016-11-12 13:33:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,10 +27,7 @@ var comment123 = [] ;
      
          };
                            
-
-
-                                
-                     
+        
                                 
  var img = document.getElementById ('madi');
 var pos = 2;
